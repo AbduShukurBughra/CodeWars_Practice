f8eb3771cf4d27e953fe293aa03e4b1838f10cba
AbduShukur <abdushukur.sawut@gmail.com>
2024-05-14 00:36:32 +0300

    Calculate

--- src/kyu8/Calculate.java
+++ src/kyu8/Calculate.java
@@ -15,6 +15,27 @@ if bmi > 30 return "Obese"
 Categories  : Fundamentals
  */
 public class Calculate {
+    public static void main(String[] args) {
+        double weight = 70;
+        double height = 1.75;
 
+        String result = bmi(weight,height);
+        System.out.println("BMI is " + result);
+    }
+    
+    public static String bmi(double weight, double height) {
+        
+        double bmi = (weight / (height * height)*100);
+        
+        if (bmi <= 18.5){
+            return "Underweight";
+        } else if (bmi<=25.0) {
+            return "Normal";
+        } else if (bmi<=30.0) {
+            return "Overweight";
+        }else {
+            return "Obese";
+        }
+    }
 
 }
